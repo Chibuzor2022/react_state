8b5d4629726f085d7e748ccfb3d6db239970dc24
chibuzor <incovenant@hotmail.com>
2025-04-11 16:07:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class based component with person object
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			person: {
+				fullName: "Jessica Gray",
+				bio: "A Professional Software Developer who loves to build things.",
+				imgSrc: "/profileImage.jpg",
+				profession: "Software Developer",
+			},
+			shows: false,
+			timeSinceMount: 0,
+		};
+
+		this.toggleShow = this.toggleShow.bind(this);
+	}
+
+	componentDidMount() {
+		// // Start a timer that runs every 1000 milliseconds
+
+		this.interval = setInterval(() => {
+			// Update the state by incrementing 'timeSinceMount' by 1
+
+			this.setState((prevState) => ({
+				timeSinceMount: prevState.timeSinceMount + 1,
+			}));
+		}, 1000);
+	}
+	// to clear the interval
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	// to update the 'shows' value to its opposite
+	toggleShow() {
+		this.setState((prevState) => ({
+			shows: !prevState.shows,
+		}));
+	}
+
+	render() {
+		const { person, shows, timeSinceMount } = this.state;
+
+		return (
+			<div className="App" style={{ padding: "12px", textAlign: "center" }}>
+				<h1>Person Profile</h1>
+
+				{/* toggle button to show/hide the profile */}
+
+				<button onClick={this.toggleShow}>
+					{shows ? "Hide Profile" : "Show Profile"}
+				</button>
+
+				{/* to show the profile image and details */}
+				{shows && (
+					<div style={{ marginTop: "10px" }} className="profile">
+						<img
+							src={person.imgSrc}
+							alt="Profile"
+							style={{ width: "130px", borderRadius: "15px" }}
+						/>
+						<h2>{person.fullName}</h2>
+						<h4>{person.profession}</h4>
+						<p>{person.bio}</p>
+					</div>
+				)}
+
+				{/* to show the  time interval since the last component was mounted */}
+				<p style={{ marginTop: "30px" }}>
+					Time since component mounted: {timeSinceMount} seconds
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
